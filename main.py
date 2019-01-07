43473db1eaf0f485a0b523567b5eb8fead64ab2a
ketsu8 <ketsoo@inbox.ru>
2019-01-08 01:34:39 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,107 @@
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+from qcodeedit import QCodeEdit
+
+__resourcesDirectory__ = 'resources/'
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUI()
+
+    def setupEditor(self):
+        self.editor = QCodeEdit()
+    
+    def setupCompleter(self):
+        self.statusBar().showMessage('Setting-up completer...')
+        self.completer = QCompleter(self)
+        self.completer.setModelSorting(QCompleter.CaseInsensitivelySortedModel)
+        self.completer.setCaseSensitivity(Qt.CaseInsensitive)
+        self.completer.setWrapAround(False)
+        self.completer.setModel(QStringListModel([], self.completer))
+        self.editor.setCompleter(self.completer)
+
+    def setupToolbar(self):
+        self.statusBar().showMessage('Setting-up toolbar...')
+        self.toolbar = QToolBar('Toolbar')
+        self.toolbar.setStyleSheet('padding: 8px; background: #333333; border-radius: 0px; spacing: 15px;')
+        self.addToolBar(Qt.LeftToolBarArea, self.toolbar)
+        self.toolbar.setMovable(False)
+
+        self.toolbar.addAction(QIcon(__resourcesDirectory__ + 'icons/run.png'), 'Build and Run')
+        self.toolbar.addAction(QIcon(__resourcesDirectory__ + 'icons/package.png'), 'Build in package')
+        self.toolbar.addAction(QIcon(__resourcesDirectory__ + 'icons/settings.png'), 'Project Settings')
+        self.toolbar.addAction(QIcon(__resourcesDirectory__ + 'icons/open.png'), 'Open Project')
+        self.toolbar.addAction(QIcon(__resourcesDirectory__ + 'icons/save.png'), 'Save Project')
+
+    def setupTipsList(self):
+        self.statusBar().showMessage('Setting-up tips list...')
+        self.tips = QListView()
+        self.tips.setStyleSheet('color: #ccc; padding: 10px; selection-background-color: #37373D; background: #252526; border-radius: 0px;')
+        model = QStandardItemModel()
+        self.tips.setModel(model)
+
+    def setupMenubar(self):
+        self.statusBar().showMessage('Setting-up menubar...')
+        self.menuBar().setStyleSheet('color: #ccc; background: #3A3935; border-radius: 0px; min-height: 25px; spacing: 18px; selection-background-color: #37373D;')
+        fileMenu = self.menuBar().addMenu('File')
+        fileMenu.addAction('Save')
+        
+        editMenu = self.menuBar().addMenu('Edit')
+        editMenu.addAction('Undo', lambda: self.editor.undo(), 'Ctrl+Z')
+        editMenu.addAction('Redo', lambda: self.editor.redo(), 'Ctrl+Y')
+        editMenu.addSeparator()
+        editMenu.addAction('Cut', lambda: self.editor.cut(), 'Ctrl+X')
+        editMenu.addAction('Copy', lambda: self.editor.copy(), 'Ctrl+C')
+        editMenu.addAction('Paste', lambda: self.editor.paste(), 'Ctrl+V')
+
+
+        selectionMenu = self.menuBar().addMenu('Selection')
+        selectionMenu.addAction('Select All', lambda: self.editor.selectAll(), 'Ctrl+A')
+
+        viewMenu = self.menuBar().addMenu('View')
+        viewMenu.addAction('Toggle Toolbar', lambda: self.toolbar.setVisible(not self.toolbar.isVisible()), 'Ctrl+B')
+        viewMenu.addAction('Toggle Tips Panel', lambda: self.tips.setVisible(not self.tips.isVisible()), 'Ctrl+J')
+        viewMenu.addAction('Toggle Statusbar', lambda: self.statusBar().setVisible(not self.statusBar().isVisible()), 'Ctrl+M')
+
+        helpMenu = self.menuBar().addMenu('Help')
+        helpMenu.addAction('About ' + QCoreApplication.applicationName(), lambda: QMessageBox.about(self, 'About ' + QCoreApplication.applicationName(), 'Pretty development IDE.\nVersion: ' + QCoreApplication.applicationVersion()))
+        helpMenu.addAction('About Qt', lambda: QMessageBox.aboutQt(self, 'About Qt'))
+
+    def setupSplitter(self):
+        self.statusBar().showMessage('Setting-up splitter...')
+        self.splitter = QSplitter()
+        self.splitter.setStyleSheet('background: #252526; border-radius: 0px;')
+        self.splitter.setOrientation(Qt.Orientation.Vertical)
+        self.splitter.addWidget(self.editor)
+        self.splitter.addWidget(self.tips)
+
+    def setupStatusbar(self):
+        self.statusBar().setStyleSheet('color: #ccc; spacing: 15px; background: #A700C5; border-radius: 0px;')
+        self.statusBar().showMessage('Waiting...')
+
+    def setupUI(self):
+        self.setupStatusbar()
+        self.setupEditor()
+        self.setupToolbar()
+        self.setupTipsList()
+        self.setupMenubar()
+        self.setupCompleter()
+        self.setupSplitter()
+        self.setCentralWidget(self.splitter)
+        self.statusBar().showMessage('All set!')
+        self.setWindowTitle(QCoreApplication.applicationName())
+
+if __name__ == '__main__':
+    QCoreApplication.setApplicationName('Prettycode')
+    QCoreApplication.setOrganizationDomain('ketsu8')
+    QCoreApplication.setOrganizationName('Ketsu8')
+    QCoreApplication.setApplicationVersion('dev0')
+
+    app = QApplication([])
+    window = MainWindow()
+    window.setMinimumSize(640, 512)
+    window.show()
+    app.exec_()
---
+++ qcodeedit.py
@@ -0,0 +1,102 @@
+
+from platform import system as platform
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+from jedi import Script
+
+
+class QCodeEdit(QTextEdit):
+    def __init__(self, parent=None):
+        super(QCodeEdit, self).__init__(parent)
+
+        self._completer = None
+        self.lastItems = []
+
+        self.setPlainText(
+                "This TextEdit provides autocompletions for words that have "
+                "more than 3 characters. You can trigger autocompletion "
+                "using %s" % QKeySequence("Ctrl+E").toString(
+                        QKeySequence.NativeText))
+        font = QFont()
+        font.setFamily('Menlo' if platform() == 'Darwin' else ('Consoleas' if platform() == 'Windows' else 'Courier'))
+        font.setFixedPitch(True)
+        font.setPointSize(13 if platform() == 'Darwin' else 10)
+
+        self.setFont(font)
+        self.setStyleSheet('color: #ccc; padding: 10px; background: #1E1E1E; border-radius: 0px')
+        self.setPlaceholderText('Maybe, you should write a couple of lines of code here...')
+        self.setTabStopDistance(35)
+        self.setPlainText('from matrix import *\n\nclass MyApp(App):\n\tdef setup(self):\n\t\tpass\n\n\tdef frame(self):\n\t\tpass\n\n\tdef on_button(self, button):\n\t\tpass')
+
+        from highlight import PythonHighlighter
+        self.highlighter = PythonHighlighter(self.document())
+
+        self.completionsTimer = QTimer(self, interval=750, timeout=self.makeCompletions)
+
+        # uncomment to use autocomplete
+        # but, now it's very unstable
+        # self.completionsTimer.start()
+
+    def setCompleter(self, c):
+        if self._completer is not None:
+            self._completer.activated.disconnect()
+
+        self._completer = c
+        c.setWidget(self)
+        c.setCompletionMode(QCompleter.PopupCompletion)
+        c.setCaseSensitivity(Qt.CaseInsensitive)
+        c.activated.connect(self.insertCompletion)
+        self._completer.popup().setStyleSheet('color: white; background: #2A2A2A; border-radius: 10px')
+
+    def makeCompletions(self):
+        currentLineNumber = self.textCursor().blockNumber()
+        currentLine = self.toPlainText().splitlines()[currentLineNumber]
+        if self.toPlainText().replace('\t', '').replace(' ', '').replace('\n', '') == '': return 
+        try:
+            items = [i.name for i in Script(source=self.toPlainText(), line=currentLineNumber + 1, column=self.textCursor().columnNumber()).completions()]
+        except ValueError: return 
+        if self.lastItems == items: return
+        self._completer.setModel(QStringListModel(items, self._completer))
+        self.lastItems = items
+        from time import time
+        print(time())
+
+    def completer(self):
+        return self._completer
+
+    def insertCompletion(self, completion):
+        if self._completer.widget() is not self:
+            return
+
+        tc = self.textCursor()
+        extra = len(completion) - len(self._completer.completionPrefix())
+        tc.movePosition(QTextCursor.Left)
+        tc.movePosition(QTextCursor.EndOfWord)
+        tc.insertText(completion[-extra:])
+        self.setTextCursor(tc)
+
+    def textUnderCursor(self):
+        tc = self.textCursor()
+        tc.select(QTextCursor.WordUnderCursor)
+
+        return tc.selectedText()
+
+    def focusInEvent(self, e):
+        if self._completer is not None:
+            self._completer.setWidget(self)
+
+        super(QCodeEdit, self).focusInEvent(e)
+
+    def keyPressEvent(self, e):
+        if self._completer is not None and self._completer.popup().isVisible():
+            if e.key() in (Qt.Key_Enter, Qt.Key_Return, Qt.Key_Escape, Qt.Key_Tab, Qt.Key_Backtab):
+                e.ignore()
+                return
+        
+        super(QCodeEdit, self).keyPressEvent(e)
+        cr = self.cursorRect()
+        cr.setWidth(self._completer.popup().sizeHintForColumn(0) + self._completer.popup().verticalScrollBar().sizeHint().width())
+        self._completer.complete(cr)
