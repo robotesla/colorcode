70f277b4b6d48a5613eb14b3f3027d3a30121a2b
ketsu8 <ketsoo@inbox.ru>
2019-01-09 01:14:25 +0300

    Added splash screen

--- main.py
+++ main.py
@@ -115,14 +115,35 @@ if __name__ == '__main__':
     QCoreApplication.setApplicationName('Prettycode')
     QCoreApplication.setOrganizationDomain('ketsu8')
     QCoreApplication.setOrganizationName('Ketsu8')
-    QCoreApplication.setApplicationVersion('dev0')
+    QCoreApplication.setApplicationVersion('0.0.1a')
     app = QApplication([])
 
     from qtmodern.styles import dark
     dark(app)
+
+    splashPicture = QPixmap(__resourcesDirectory__ + 'splash.png')
+    splashScreen = QSplashScreen(splashPicture, Qt.WindowStaysOnTopHint)
+    splashScreen.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+    splashScreen.setEnabled(False)
+
+    progressBar = QProgressBar(splashScreen)
+    progressBar.setMaximum(10)
+    progressBar.setTextVisible(False)
+    progressBar.setGeometry(160, 250, 200, 20)
+    splashScreen.showMessage('Version: <b>' + QCoreApplication.applicationVersion() + '</b>', Qt.AlignBottom | Qt.AlignRight, Qt.white)
     
+    splashScreen.show()
+
+    from time import time
+    for i in range(1, 11):
+        progressBar.setValue(i)
+        t = time()
+        while time() < t + 0.2:
+            app.processEvents()
+
     window = MainWindow()
     window.setMinimumSize(640, 512)
     window.show()
+    splashScreen.finish(window)
     app.exec_()
     
