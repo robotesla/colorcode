b057d9a4d4cd8efa70489a2ddd321356b3acf92d
ketsu8 <ketsoo@inbox.ru>
2019-01-12 02:35:51 +0300

    Fixing settings bug

--- translations/__init__.py
+++ translations/__init__.py
@@ -7,19 +7,19 @@ from PySide2.QtCore import QSettings
 from PySide2.QtWidgets import QApplication, QMainWindow, QMessageBox
 
 path = join(dirname(argv[0]), 'translations')
+settings = QSettings()
 
 languages = []
 for language in listdir(path):
     if language.startswith('__'): continue
     languages.append(language)
 
-language = QSettings().value('interface/language', 'en')
+language = settings.value('interface/language', 'en')
 
 def returnLanguage(language):
     return translation('prettycode', path, [language]).gettext
 
 def setLanguage(language):
-    settings = QSettings()
     settings.setValue('interface/language', language)
     settings.sync()
     QMessageBox.information(QMainWindow(), 'Setting Language', 'Language successfuly setted. Application need to reload.')
