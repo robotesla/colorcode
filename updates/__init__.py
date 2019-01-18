a91726e8e14bb3f8549211c0c3a93dfae0cddceb
ketsu8 <ketsoo@inbox.ru>
2019-01-18 06:31:59 +0300

    Added beautifuly spaces at updates module

--- updates/__init__.py
+++ updates/__init__.py
@@ -4,11 +4,13 @@ from settings import preReleasesEnable
 def checkUpdatesAvailabe():
     from requests import get
     releasesInformation = get('https://api.github.com/repos/ketsu8/prettycode/releases').json()
+    
     if preReleasesEnable == True:
         latestReleaseInformation = releasesInformation[0]
     else:
         for release in releasesInformation:
             if release['prerelease'] == True: continue
             latestReleaseInformation = release
+
     currentVersion = QCoreApplication.applicationVersion()
     return False if latestReleaseInformation['tag_name'] == currentVersion else True
