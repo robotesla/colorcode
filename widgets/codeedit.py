9680fc58e955340497924c3ed5ca1d959ff34f3e
Ilya Breitburg <ilya.breytburg@gmail.com>
2019-02-21 11:36:18 +0300

    Added translation to placeholder text at editor

--- widgets/codeedit.py
+++ widgets/codeedit.py
@@ -25,7 +25,7 @@ class QCodeEdit(QPlainTextEdit):
         from settings import autoCompleteEnable
         self.setStyleSheet('color: white; padding: 10px; background: #1E1E1E; border-radius: 0px')
         
-        self.setPlaceholderText('Maybe, you should write a couple of lines of code here...')
+        self.setPlaceholderText(_('Maybe, you should write a couple of lines of code here...'))
         self.setTabStopDistance(35)
         self.setPlainText('from matrix import *\n\ndef setup():\n\tpass\n\ndef frame():\n\tpass')
 
